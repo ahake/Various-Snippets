930d264f3631d0e7924b41c58b8b53e813f3de6e
Andy Hake <ahake@users.noreply.github.com>
2020-06-27 10:25:16 +1000

    Add files via upload
    A JS bookmark that opens today's Border Mail (local newspaper)

---
+++ BorderMailToday.js
@@ -0,0 +1,20 @@
+javascript:function url() {
+  var date = new Date();
+  var y = date.getFullYear();
+  var m = date.getMonth() + 1;
+  if (m < 10) {
+    m = '0' + m;
+  }
+  var d = date.getDate();
+  if (d < 10) {
+    d = '0' + d;
+  }
+  var date = y + '/' + m + '/' + d;
+
+  return (
+    'https://digital-print-edition.austcommunitymedia.com.au/BMM/' +
+    date +
+    '/3dissue/index.html'
+  );
+}
+window.open(url(), '_blank');
